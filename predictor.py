630dd729f79c84e8c134bb92638f82ba7d34f4d7
Rincy57 <143395617+Rincy57@users.noreply.github.com>
2026-08-19 11:53:42 +0530

    Add files via upload

---
+++ predictor.py
@@ -0,0 +1,40 @@
+import joblib
+import pandas as pd
+
+
+pipeline = joblib.load("model/pipeline.pkl")
+
+
+def predict(data: dict):
+    
+    df = pd.DataFrame([data])
+
+    
+    prediction = pipeline.predict(df)[0]
+
+  
+    probability = pipeline.predict_proba(df)[0].tolist()
+
+   
+
+    if probability[0] > 0.5:
+        return {
+            "prediction": int(prediction),
+            'verdict':"This  passenger will not survive",
+            "probability": {
+                "No survival": probability[0],
+                "Survival": probability[1]
+            }
+        }
+
+
+    else:    
+            return {
+                "prediction": int(prediction),
+                'verdict':"This  passenger will survive",
+                "probability": {
+                    "No survival": probability[0],
+                    "Survival": probability[1]
+                }
+            }
+    
